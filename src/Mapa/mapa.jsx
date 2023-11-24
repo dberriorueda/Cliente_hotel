5260d2f5d0fb2720631e78c3404aacabefc8355c
Diego <dberriorueda@gmail.com>
2023-11-24 00:14:51 -0500

    Agregue mapa

---
+++ src/Mapa/mapa.jsx
@@ -0,0 +1,9 @@
+import './Mapa.css';
+export function Mapa() {
+    return (
+        <div className="mapa-container">
+            <h2>Ubicacion del hotel</h2>
+            <img src="../../src/assets/mapa.jpg" alt="mapa de medellin" className="mapa-imagen"/>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Home } from './Home/Home';
 import { Carrusel } from './Carrusel/Carousel';
+import { Mapa } from './mapa/mapa';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
@@ -10,5 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home />
     <Carrusel/>
+    <Mapa/>
   </React.StrictMode>,
 )
